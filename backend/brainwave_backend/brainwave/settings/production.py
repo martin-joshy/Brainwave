c79233cef421498a0481b13dbf3235dd8a70b4a0
martin-joshy <martinjoshy28@gmail.com>
2024-10-25 15:28:27 +0530

    refactor: modulize the settings.py for production and development and point to the respective settings.

---
+++ backend/brainwave_backend/brainwave/settings/production.py
@@ -0,0 +1,13 @@
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": "5432",
+    }
+}
