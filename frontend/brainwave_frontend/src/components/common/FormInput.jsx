fea5528e2033aee17b37acc9dea21d62677ade61
martin-joshy <martinjoshy28@gmail.com>
2024-10-28 09:31:40 +0530

    fix: adjust the styling of the form input to be more responsive

--- frontend/brainwave_frontend/src/components/common/FormInput.jsx
+++ frontend/brainwave_frontend/src/components/common/FormInput.jsx
@@ -7,31 +7,58 @@ const FormInput = ({ name, type, placeholder }) => {
 
   return (
     <div
-      className={`form-group w-full flex flex-col items-center py-4 ${hasError ? 'py-1' : 'py-4'}`}
+      className={`form-group w-full flex flex-col items-center ${hasError ? 'pt-2' : 'pt-4'}`}
     >
       <Field
-        className="w-full [border:none] bg-[transparent] self-stretch h-[31.1px] font-h5-regular text-smi-1 text-character-disabled-placeholder-25 z-[3]"
+        className="w-full [border:none] bg-[transparent] self-stretch font-h5-regular text-smi-1 text-character-disabled-placeholder-25 z-[3]"
         type={type}
         name={name}
         placeholder={placeholder}
         as={TextField}
         variant="outlined"
         sx={{
-          '& fieldset': { borderColor: hasError ? '#D32F2F' : '#d9d9d9' },
+          '& fieldset': {
+            borderColor: hasError ? '#D32F2F' : '#d9d9d9',
+          },
           '& .MuiInputBase-root': {
-            height: '43.1px',
+            height: '43px',
             backgroundColor: '#fff',
             borderRadius: '6.82px',
             fontSize: '14.1px',
+            '& input': {
+              padding: '16px 14px',
+              boxSizing: 'border-box',
+              height: '100%',
+              '&:-webkit-autofill': {
+                transition: 'background-color 5000s ease-in-out 0s',
+                boxShadow: 'none',
+                WebkitTextFillColor: '#000000',
+              },
+            },
           },
-          '& .MuiInputBase-input': { color: '#000000' },
-          '& .MuiInputBase-input::placeholder': { color: 'rgba(0, 0, 0, 0.5)' },
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderWidth: '1px',
+            },
+            '&:hover fieldset': {
+              borderWidth: '1px',
+            },
+            '&.Mui-focused fieldset': {
+              borderWidth: '1px',
+            },
+          },
+          '& .MuiInputBase-input': {
+            color: '#000000',
+          },
+          '& .MuiInputBase-input::placeholder': {
+            color: 'rgba(0, 0, 0, 0.5)',
+          },
         }}
       />
       <ErrorMessage
         name={name}
         component="div"
-        className="text-red-600 font-inter font-extralight py-3 text-left w-full "
+        className="text-red-600 font-inter font-extralight text-left w-full"
       />
     </div>
   );
