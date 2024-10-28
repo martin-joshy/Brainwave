c1ed586b7db7b3043cdab9fdb150bd0b58d1f1b4
martin-joshy <martinjoshy28@gmail.com>
2024-10-28 15:17:21 +0530

    fix: Remove the extra padding in screen view

--- frontend/brainwave_frontend/src/components/user/AuthFormContainer.jsx
+++ frontend/brainwave_frontend/src/components/user/AuthFormContainer.jsx
@@ -2,11 +2,10 @@ import { Container } from '@mui/material';
 
 function AuthFormContainer({ Form }) {
   return (
-    <Container>
+    <Container className="sm:px-4">
       <div className="min-h-screen p-2 flex items-center justify-center">
         <div className="w-full max-w-7xl">
-          <div className="relative w-full flex flex-col shadow-[0px_3px_20.25px_rgba(0,_0,_0,_0.25)] [backdrop-filter:blur(3.03px)] rounded-[39.43px] bg-powderblue overflow-hidden">
-            {/* Background image for the container */}
+          <div className="relative w-full flex flex-col shadow-[0px_3px_20.25px_rgba(0,_0,_0,_0.25)] [backdrop-filter:blur(3.03px)] rounded-[39.43px]  bg-powderblue overflow-hidden">
             <div className="absolute inset-0 z-0">
               <img
                 className="w-full h-full object-cover"
@@ -15,11 +14,8 @@ function AuthFormContainer({ Form }) {
               />
             </div>
 
-            {/* Main content area */}
-            <div className="relative z-10 flex min-h-[400px]">
-              {/* Form section */}
+            <div className="relative z-10 flex min-h-[100vh]">
               <div className="flex-1 md-min:w-1/2">
-                {/* Logo section */}
                 <div className="relative z-10 pt-8 pl-8 md-max:flex md-max:justify-center">
                   <div className="w-28 h-12 relative">
                     <img
@@ -34,7 +30,6 @@ function AuthFormContainer({ Form }) {
                   <Form />
                 </div>
               </div>
-              {/* Right side image */}
               <div className="md-min:w-1/2 md-max:hidden">
                 <img
                   className="h-full w-full object-cover rounded-tr-[39.43px] rounded-br-[39.43px]"
